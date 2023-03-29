e2e244a12fda1419e1308163a99fd0e5db2d0652
Saurabh100kar <saurabh03105@gamil.com>
2023-03-29 19:24:59 +0530

    day-2

--- my-glow/src/Pages/Login.jsx
+++ my-glow/src/Pages/Login.jsx
@@ -1,1 +1,54 @@
-Saurabh
+// Saurabh
+
+  import { useNavigate } from "react-router-dom";
+  
+  function Login() {
+    const navigate = useNavigate();
+    return (
+      <Stack
+        minH={"100vh"}
+        direction={{ base: "column", md: "row" }}
+        backgroundColor="#f2f2f2 "
+      >
+        <Flex p={8} flex={1} align={"center"} justify={"center"}>
+          <Stack spacing={4} w={"full"} maxW={"md"}>
+            <Heading fontSize={"2xl"}>Sign in to your account</Heading>
+            <FormControl id="email">
+              <FormLabel>Email address</FormLabel>
+              <Input type="email" />
+            </FormControl>
+            <FormControl id="password">
+              <FormLabel>Password</FormLabel>
+              <Input type="password" />
+            </FormControl>
+            <Stack spacing={6}>
+              <Stack
+                direction={{ base: "column", sm: "row" }}
+                align={"start"}
+                justify={"space-between"}
+              >
+                <Checkbox>Remember me</Checkbox>
+                <Link color={"blue.500"}>Forgot password?</Link>
+              </Stack>
+              <Button
+                colorScheme={"blue"}
+                variant={"solid"}
+                onClick={() => navigate("/")}
+              >
+                Sign in
+              </Button>
+            </Stack>
+          </Stack>
+        </Flex>
+        <Flex flex={1}>
+          <Image
+            alt={"Login Image"}
+            objectFit={"cover"}
+            src={"C:\Users\Saurabh Shambharkar\OneDrive\Desktop\Unit-5-Project\noisy-mailbox-2939\my-glow\src\logo.svg"}
+          />
+        </Flex>
+      </Stack>
+    );
+  }
+  
+  export default Login;
