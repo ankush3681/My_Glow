073733104dbb00de64d6afb8e81ca6ee439f8c16
SagarN21 <karwarbinaga@gmail.com>
2023-03-31 19:02:52 +0530

    post done

---
+++ my-glow/src/Pages/EditDeletePage.jsx
@@ -0,0 +1,108 @@
+
+import { Box, Heading, Image, Text, Tag, Grid, Button,Flex, Spacer,useToast } from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { get_products } from "../Redux/productReducer/action";
+import { BsStarHalf,BsCart4,BsCart2 } from "react-icons/bs";
+
+export const EditDeletePage = () => {
+
+   
+
+
+    const [cartIcon,setCartIcon] = useState(false);
+    const [page,setPage] = useState(1);
+  const dispatch = useDispatch();
+  const { products } = useSelector((state) => {
+    // console.log(state.productReducer.products);
+    return state.productReducer;
+  });
+  const toast = useToast();
+
+  const handleCart = () =>{
+       setCartIcon(prev => !prev)
+
+       toast({
+        position:'top',
+        title: 'Item Added.',
+        description: "You can check your item in cart section.",
+        status: 'success',
+        duration: 2000,
+        isClosable: true,
+      })
+  }
+
+  let count=false;
+
+  useEffect(() => {
+    dispatch(get_products);
+  }, []);
+  return (
+    <Box w="70%" ml="4rem">
+    <Box  >
+      <Heading color="red" mb={"5%"}>
+       Admin Panel
+      </Heading>
+      <Grid templateColumns="repeat(3, 1fr)" gap={10}>
+        {products.length !== 0 &&
+          products.map((item) => {
+            return (
+              <Box
+                key={item.id}
+                boxShadow="rgba(0, 0, 0, 0.1) 0px 4px 12px"
+                textAlign="center"
+                p={"1rem .5rem"}
+                borderRadius="8px"
+                lineHeight={"25px"}
+              >
+                <Image src={item.image1} height="15rem" m="auto" />
+                <Text>{item.title}</Text>
+                <Text>{item.brand}</Text>
+                <Tag
+                  d={"inline"}
+                  bg="white"
+                  color="grey"
+                  textDecoration="line-through"
+                  fontSize={".8rem"}
+                >
+                  Rs.200
+                </Tag>
+                <Tag d={"inline"} bg="white">
+                  Rs.{item.price}
+                </Tag>
+                <Tag d={"inline"} bg="white" color="pink.300">
+                  {item.discount}%Off
+                </Tag>
+
+                <Text>
+                  {item.rating}{" "}
+                  <Tag bg="white">
+                    <BsStarHalf color="red" />
+                  </Tag>
+                </Text>
+
+                <Flex w="90%" m="auto">
+                    <Button fontSize="1.5rem"  bg="pink.100" onClick={handleCart}>
+                      Edit
+                        </Button>
+                    <Spacer/>
+                    <Button p={"1rem 2rem"}  bg="pink.100"> <Link to={`/product/${item.id}`}>Delete</Link></Button>
+                </Flex>
+              </Box>
+            );
+          })}
+      </Grid>
+      <Box m={"2rem 1rem"}>
+        <Button  bg="pink.100">Previous</Button>
+        <Button  mr={"1rem"} ml={"1rem"}>{page}</Button>
+        <Button  bg="pink.100">Next</Button>
+      </Box>
+    </Box>
+    </Box>
+  );
+};
+
+
+  
+
--- my-glow/src/Redux/productReducer/action.js
+++ my-glow/src/Redux/productReducer/action.js
@@ -1,19 +1,33 @@
-import { 
-    GETPRODUCTSUCCESSSSTATUS, 
-    PRODUCTFAILEDSTATUS, 
-    PRODUCTREQUESTSTATUS 
+import {
+  GETPRODUCTSUCCESSSSTATUS,
+  POSTPRODUCTSUCCESS,
+  PRODUCTFAILEDSTATUS,
+  PRODUCTREQUESTSTATUS,
 } from "./actionType";
 import axios from "axios";
 
+const post_product_success = () => {
+  return { type: POSTPRODUCTSUCCESS };
+};
 
-export const get_products = (dispatch) =>{
-   dispatch({type:PRODUCTREQUESTSTATUS})
- axios.get(`https://lime-repulsive-cockroach.cyclic.app/myglow`)
- .then((res)=>{
-    // console.log(res.data);
-    dispatch({type:GETPRODUCTSUCCESSSSTATUS,payload:res.data})
- })
- .catch(()=>{
-   dispatch({type:PRODUCTFAILEDSTATUS})
- })
-}
+export const get_products = (dispatch) => {
+  dispatch({ type: PRODUCTREQUESTSTATUS });
+  axios
+    .get(`https://lime-repulsive-cockroach.cyclic.app/myglow`)
+    .then((res) => {
+      // console.log(res.data);
+      dispatch({ type: GETPRODUCTSUCCESSSSTATUS, payload: res.data });
+    })
+    .catch(() => {
+      dispatch({ type: PRODUCTFAILEDSTATUS });
+    });
+};
+
+export const postProduct = (data) => (dispatch) => {
+  dispatch({ type: PRODUCTREQUESTSTATUS });
+
+  axios
+    .post(`https://glow-iw5x.onrender.com/myglow`, data)
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err.response.data));
+};
