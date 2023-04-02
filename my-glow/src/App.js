8a05b0d95675da0efa84b050f61b251a1c8f89c5
Saurabh100kar <saurabh03105@gamil.com>
2023-04-02 16:19:18 +0530

    done login and signup

--- my-glow/src/App.js
+++ my-glow/src/App.js
@@ -10,7 +10,7 @@ function App() {
     <div className="App">
       <Navbar/>
       <MainRoutes/> 
-      <Footer/>
+      {/* <Footer/> */}
   
     </div>
   );
--- my-glow/src/Pages/MainRoutes.jsx
+++ my-glow/src/Pages/MainRoutes.jsx
@@ -1,33 +1,31 @@
-import React from 'react';
+import React from "react";
 import HomePage from "./HomePage";
-import { Routes,Route } from "react-router-dom";
-import Login from './Login';
-import Signup from './Signup';
-import { Admin } from './Admin';
-import GlowProduct from './GlowProducts';
-import {ProductDetail} from './ProductDetail';
-import Cart from './Cart';
+import { Routes, Route } from "react-router-dom";
+import Login from "./Login";
+import Signup from "./Signup";
+import { Admin } from "./Admin";
+import GlowProduct from "./GlowProducts";
+import { ProductDetail } from "./ProductDetail";
+import Cart from "./Cart";
 
-import {Payment} from './Payment';
+import { Payment } from "./Payment";
 
-
-import { EditDeletePage } from './EditDeletePage';
-
+import { EditDeletePage } from "./EditDeletePage";
 
 const MainRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<HomePage/>}></Route>
-      <Route path="/login" element={<Login/>}></Route>
-      <Route path="/signup" element={<Signup/>}></Route>
-      <Route path="/admin" element={<Admin/>}></Route>
-      <Route path="/product" element={<GlowProduct/>}></Route>
-      <Route path="/product/:id" element={<ProductDetail/>}></Route>
-      <Route path="/payment" element={<Payment/>}></Route>
-      <Route path="/cart" element={<Cart/>}></Route>
-      <Route path='/edit' element={<EditDeletePage/>}></Route>
+      <Route path="/" element={<HomePage />}></Route>
+      <Route path="/login" element={<Login />}></Route>
+      <Route path="/signup" element={<Signup />}></Route>
+      <Route path="/admin" element={<Admin />}></Route>
+      <Route path="/product" element={<GlowProduct />}></Route>
+      <Route path="/product/:id" element={<ProductDetail />}></Route>
+      <Route path="/payment" element={<Payment />}></Route>
+      <Route path="/cart" element={<Cart />}></Route>
+      <Route path="/edit" element={<EditDeletePage />}></Route>
     </Routes>
-  )
-}
+  );
+};
 
 export default MainRoutes;
